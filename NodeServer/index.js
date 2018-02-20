dcdef14d297c92f9aafa2c1e3e5de4c25571f3d8
Joba <admin@serverlein.de>
2018-02-20 09:15:12 +0100

    Absolute Path for Linux

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.use(express.static('www'));
+app.use(express.static(__dirname+ '/www'));
 
 var userlist = [];
 
